3f21c47c5c7b43ab67c6bf8a8e1362b1e9338875
Prakash Dantani <dantaniprakash08@gmail.com>
2023-12-26 06:30:18 +0530

    Refactor Move Entity Interface

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -11,7 +11,7 @@ import { MdPhoneIphone } from "react-icons/md";
 import { IconType } from "react-icons";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
-import { Platform } from "../hooks/usePlatforms";
+import { Platform } from "../entities/Platform";
 interface PlateformIconProps {
   platforms: Platform[];
 }
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -2,14 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genrs";
 import APIClient from "../services/api-client";
 import ms from "ms";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>('/genres');
-export interface Genre{
-    id:number,
-    name:string,
-    image_background:string
-}
-
 const useGenre = () => useQuery({
     queryKey:['genres'],
     queryFn: apiClient.getAll,
